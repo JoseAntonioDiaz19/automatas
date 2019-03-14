7b248ac3f9e46352bd4434e200a0af401d8008a0
Dizan <Dizan@Dizan-PC>
2019-03-14 09:21:37 -0600

    Se mejorado el formato del codigo

--- src/algoritmos/AlgoritmoExponenciales.java
+++ src/algoritmos/AlgoritmoExponenciales.java
@@ -17,7 +17,7 @@ public class AlgoritmoExponenciales {
     private char S;
     private Tabla_Numeros_Exponenciales tablaNumerosExponenciales;
     private char[] arregloCadena_a_Char;
-    private int i = 0;
+    private int i;
     
     /**
      * @return El arregloCadena_a_Char
@@ -40,18 +40,39 @@ public class AlgoritmoExponenciales {
     private char Leer_Simbolo(int i){
         return getArregloCadena_a_Char()[i];
     }
-
+    /**
+     * El metodo proceso realiza todo el procedimiento para saber si 
+     * la cadena proporcionada por el usuario es válida respecto
+     * al alfabeto para numeros reales con exponente.
+     * 
+     * La variable estado mantiene el estado en el que se encuentra
+     * el analisis de la cadena (q0, q1, q2...) en todo momento.
+     * 
+     * La variable i es el indice del arreglo Tabla_Numeros_Exponenciales
+     * en el método sirve para acceder a cada uno de los simbolos de la
+     * cadena proporcionada por el usuario.
+     * 
+     * El arreglo Tabla_Numeros_Exponenciales contiene la tabla sintáctica
+     * del automata que valida numeros enteros reales con notacion exponencial
+     * para numeros decimales.
+     * 
+     * @return Un boolean que dice si la cadena es valida o erronea 
+     * true and false respectivamente.
+     */
     public boolean Proceso(){
         
-        tablaNumerosExponenciales = new Tabla_Numeros_Exponenciales();
-        i = 0;
         Estado = '0';
-
+        i = 0;
+        tablaNumerosExponenciales = new Tabla_Numeros_Exponenciales();
+        
         do{
-            if(i<getArregloCadena_a_Char().length){
-
+            if(i < getArregloCadena_a_Char().length){
+                
+                //Se asigna a S el simbolo actual para ser analizado 
                 S = Leer_Simbolo(i);
-                S = NumeroSimbolo(S);
+                
+                //Se comprueba si el simbolo a analizar es un digito
+                S = numeroSimbolo(S);
 
                 switch(S){
 
@@ -78,8 +99,10 @@ public class AlgoritmoExponenciales {
                 }
 
                 System.out.printf("%5s %5s %5s\n", Estado, S, Entrada);  
-                Estado=tablaNumerosExponenciales.F(Estado, Entrada);
+                Estado = tablaNumerosExponenciales.F(Estado, Entrada);
+                
                 i++;
+                
            }else{
 
                Entrada='F';
@@ -87,22 +110,40 @@ public class AlgoritmoExponenciales {
                Estado=tablaNumerosExponenciales.F(Estado, Entrada);
                i++;
            }
+            
        }while(Estado!= 'E' && Estado!= 'A' && i <= getArregloCadena_a_Char().length);
 
         if(Estado == 'A'){
-               return true;
+              return true;
         }
+        
         else{
                return false;
         }
     }
+    /**
+     * El metodo Error envia un mensaje si un simbolo dentro de la cadena 
+     * proporcionada por el usuario no se encuentra dentro del alfabeto.
+     * @param Simbolo es un caracter de un determinado alfabeto.
+     */
 
     public void Error(char Simbolo){
-
-        JOptionPane.showMessageDialog(null,"( "+Simbolo+ " )Simbolo No Valido", "Error",JOptionPane.ERROR_MESSAGE);
+        JOptionPane.showMessageDialog(null,
+                "( "+ Simbolo + " )Simbolo No Valido", 
+                "Error",JOptionPane.ERROR_MESSAGE);
     }
+    
+    /**
+     * El metodo numeroSimbolo comprueba si el caracter que se analiza 
+     * si el simbolo leido de la cadena proporcionada por el usuario corresponde
+     * a los numeros naturales (1, 2, 3, 4, 5, 6, 7, 8, 9, 0).
+     * @param Simbolo es un caracter de un determinado alfabeto.
+     * @return regresa un caracter char "d" si el simbolo analizado es de tipo
+     * numérico (numeros naturales) en el caso de que no sea así se retorna 
+     * el mismo simbolo recibido.
+     */
 
-    public char NumeroSimbolo(char Simbolo)
+    public char numeroSimbolo(char Simbolo)
     {
         switch(Simbolo){
             
