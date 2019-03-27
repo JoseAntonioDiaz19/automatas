6a6389f057da21832c1a6e6162298d860615f369
Dizan <Dizan@Dizan-PC>
2019-03-27 17:34:51 -0600

    Se creó una clase para controlar la interfaz grafica del proyecto 2.

---
+++ src/Controlador/proyecto2ControladorGUI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+import GUI.proyecto2GUI;
+
+/**
+ *
+ * @author Dizan
+ */
+public class proyecto2ControladorGUI {
+    
+    proyecto2GUI proyecto2GUI;
+
+    public proyecto2ControladorGUI(proyecto2GUI proyecto2GUI) {
+        this.proyecto2GUI = proyecto2GUI;
+        
+    }   
+}
